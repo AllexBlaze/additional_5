d8c6804a6c8b24debffa66809c96ff83e42ee10e
AllexBlaze <naum4ick@yandex.ru>
2018-09-27 21:11:30 +0300

    v0.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const ourStr = str;
+  let ourStack = [];
+
+  for (let i = 0, strLen = str.length; i < strLen; i++) {                    
+    
+    for (let j = 0, brConfLength = bracketsConfig.length; j < brConfLength; j++) {
+      if (ourStr[i] == bracketsConfig[j][0]) {                                
+        ourStack.push(ourStr[i]);                                             
+      } else if (ourStr[i] == bracketsConfig[j][1]) {
+          if(ourStack.length == 0 || ourStack[ourStack.length-1] != bracketsConfig[j][0]) {
+            return false;
+          } else {
+            ourStack.pop();
+          };
+        };
+    };
+  };
+
+  return ourStack.length === 0;
+};
