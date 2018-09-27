828f0f2b0044b4d361daa61b0117276ceeb4bf48
AllexBlaze <naum4ick@yandex.ru>
2018-09-27 22:19:33 +0300

    v0.2

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
   const ourStr = str;
   let ourStack = [];
-
+ 
   for (let i = 0, strLen = str.length; i < strLen; i++) {                    
     
     for (let j = 0, brConfLength = bracketsConfig.length; j < brConfLength; j++) {
-      if (ourStr[i] == bracketsConfig[j][0]) {                                
-        ourStack.push(ourStr[i]);                                             
+      if (ourStack[ourStack.length-1] == bracketsConfig[j][1]) {
+        ourStack.pop();
+        continue;
+      } 
+      if (ourStr[i] == bracketsConfig[j][0]) {  
+
+        ourStack.push(ourStr[i]);       
+
       } else if (ourStr[i] == bracketsConfig[j][1]) {
-          if(ourStack.length == 0 || ourStack[ourStack.length-1] != bracketsConfig[j][0]) {
-            return false;
+          if( ourStack.length == 0 ||
+             (ourStack[ourStack.length-1] != bracketsConfig[j][0]) ) {
+              return false;
           } else {
-            ourStack.pop();
+               ourStack.pop();
           };
         };
     };
   };
-
-  return ourStack.length === 0;
+  return ourStack.length == 0;
 };
